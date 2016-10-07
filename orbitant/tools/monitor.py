5af0b6a627385a793d3e754da7fb26d9284d1ea6
Matthew Scott <matt@11craft.com>
2016-10-07 17:55:26 -0500

    Convert numeric args to int

--- orbitant/tools/monitor.py
+++ orbitant/tools/monitor.py
@@ -12,7 +12,11 @@ def main(min_ticks: 'Ticks to accumulate before calculating' = 24,
          tpb: 'Ticks per beat' = 24,
          name: 'MIDI port name' = 'Orbitant Generator'):
     logging.info('Starting monitor; press ^C to exit')
-    monitor = clock.Monitor(min_ticks=min_ticks, max_ticks=max_ticks, tpb=tpb)
+    monitor = clock.Monitor(
+        min_ticks=int(min_ticks), 
+        max_ticks=int(max_ticks), 
+        tpb=int(tpb)
+    )
     with backend.open_input(name=name) as port:
         for message in port:
             monitor.receive_message(message)
